41b414fac974c4b75d5b8145f911641a5f21a44a
wenweiliang <wliang@genome.wustl.edu>
2018-04-25 21:19:16 +0000

    Update commend

--- variantcalling/generate_command.gatk.sh
+++ variantcalling/generate_command.gatk.sh
@@ -1,8 +1,8 @@
 ####
 #Aim: Generate google cloud command
 #Usage: bash generate_command.sh ${List_of_BAMs} ${JobName} ${DiskSize(in gb)}
-#1. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.HighPass.txt HighPass 1500
-#2. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.LowPass.txt LowPass 100
+#1. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.HighPass.txt HighPass 
+#2. bash generate_command.gatk.sh TCGA_WGS_gspath_WWL_Mar2018.LowPass.txt LowPass
 #Author: Wen-Wen Liang @ Wash U (liang.w@wustl.edu)
 ####
 
@@ -13,7 +13,10 @@ while read lines; do
 	bai=$(echo $lines | awk -F " " '{print $15}')
 	id=$(echo $lines | awk -F " " '{print $3}')
 	passvalue=$2
-	size=$3
+	size=$(echo $lines | awk -F " " '{print ($10*1.1)/1000000000}' | awk '{printf "%.0f", $1}')
+	ref=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "gs://dinglab/reference/Homo_sapiens_assembly19.fasta"; else print "gs://dinglab/reference/GRCh37-lite.fa"}')
+	dict=$(echo $ref | awk -F "." '{print $1}')
+	yaml=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "../gatk_germline.hg19.yaml"; else print "../gatk_germline.37.yaml"}')
 
-	echo "gcloud alpha genomics pipelines run --pipeline-file ../gatk_germline.yaml --inputs fafile=gs://dinglab/reference/GRCh37-lite.fa,faifile=gs://dinglab/reference/GRCh37-lite.fa.fai,dictfile=gs://dinglab/reference/GRCh37-lite.dict,bamfile=${bam},baifile=${bai},id=${id},chrlist=gs://dinglab/wliang_germlinevariantcalling/interval.list,pass=${passvalue} --outputs outputPath=gs://dinglab/wliang_germlinevariantcalling/output/gatk/${id}/ --logging gs://dinglab/wliang_germlinevariantcalling/logging/gatk/${passvalue}/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.gatk.sh
+	echo "gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs fafile=${ref},faifile=${ref}.fai,dictfile=${dict}.dict,bamfile=${bam},baifile=${bai},id=${id},chrlist=gs://dinglab/wliang_germlinevariantcalling/interval.list,pass=${passvalue} --outputs outputPath=gs://dinglab/wliang_germlinevariantcalling/output/gatk/${id}/ --logging gs://dinglab/wliang_germlinevariantcalling/logging/gatk/${passvalue}/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.gatk.sh
 done < $1
--- variantcalling/generate_command.varscan.sh
+++ variantcalling/generate_command.varscan.sh
@@ -13,7 +13,9 @@ while read lines; do
 	bai=$(echo $lines | awk -F " " '{print $15}')
 	id=$(echo $lines | awk -F " " '{print $3}')
 	passvalue=$2
-	size=$3
+	ref=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "gs://dinglab/reference/Homo_sapiens_assembly19.fasta"; else print "gs://dinglab/reference/GRCh37-lite.fa"}')
+	yaml=$(echo $lines | awk -F " " '{if ($8=="HG19_Broad_variant") print "../varscan_germline.hg19.yaml"; else print "../varscan_germline.37.yaml"}')
+	size=$(echo $lines | awk -F " " '{print ($10*1.1)/1000000000}' | awk '{printf "%.0f", $1}')
 
-	echo "gcloud alpha genomics pipelines run --pipeline-file ../varscan_germline.yaml --inputs fafile=gs://dinglab/reference/GRCh37-lite.fa,faifile=gs://dinglab/reference/GRCh37-lite.fa.fai,bamfile=${bam},baifile=${bai},id=${id},chrlist=gs://dinglab/wliang_germlinevariantcalling/chrlist.txt,pass=${passvalue} --outputs outputPath=gs://dinglab/wliang_germlinevariantcalling/output/varscan/${id}/ --logging gs://dinglab/wliang_germlinevariantcalling/logging/varscan/${passvalue}/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.varscan.sh
+	echo "gcloud alpha genomics pipelines run --pipeline-file ${yaml} --inputs fafile=${ref},faifile=${ref}.fai,bamfile=${bam},baifile=${bai},id=${id},chrlist=gs://dinglab/wliang_germlinevariantcalling/chrlist.txt,pass=${passvalue} --outputs outputPath=gs://dinglab/wliang_germlinevariantcalling/output/varscan/${id}/ --logging gs://dinglab/wliang_germlinevariantcalling/logging/varscan/${passvalue}/ --disk-size datadisk:${size}" >> cloud/gcloud_command.$2.varscan.sh
 done < $1
