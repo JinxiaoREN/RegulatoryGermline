b990c00ea846e009ff6aa8621e64db53429afb65
wenweiliang <wliang@genome.wustl.edu>
2018-04-30 20:50:41 +0000

    Run mosdepth for WXS: xaa

--- analysis/ENCODE_coverage/WXS/generate_command.sh
+++ analysis/ENCODE_coverage/WXS/generate_command.sh
@@ -10,6 +10,7 @@ while read lines; do
 	bam=$(echo $lines | awk -F " " '{print $5}')
 	bai=$(echo $lines | awk -F " " '{print $5}').bai
 	id=$(echo $lines | awk -F " " '{print $2}')
+	size=$(echo $lines | awk -F " " '{print ($6*1.1)/1000000000}' | awk '{printf "%.0f", $1}')
 
-	echo "gcloud alpha genomics pipelines run --pipeline-file site_coverage.yaml --inputs bedfile=gs://dinglab/wliang_ENCODEcov/reference/final.uniq.ENCODE.sorted.bed,bamfile=${bam},baifile=${bai},id=${id} --outputs outputPath=gs://dinglab/wliang_ENCODEcov/output/WXS/ --logging gs://dinglab/wliang_ENCODEcov/logging" >> gcloud_command.$2.sh
+	echo "gcloud alpha genomics pipelines run --pipeline-file site_coverage.yaml --inputs bedfile=gs://dinglab/wliang_ENCODEcov/reference/final.uniq.ENCODE.mirna.sorted.bed,bamfile=${bam},baifile=${bai},id=${id} --outputs outputPath=gs://dinglab/wliang_ENCODEcov/output/WXS/ --logging gs://dinglab/wliang_ENCODEcov/logging --disk-size datadisk:${size}"  >> gcloud_command.$2.sh
 done < $1
