f0eb9f2ec2e7e92ec1bc69b8453516bdecc4b47a
wenweiliang <wliang@genome.wustl.edu>
2018-05-01 20:25:59 +0000

    merge

--- variantcalling/reference/filter_gatk_varscan.cloud.pl
+++ variantcalling/reference/filter_gatk_varscan.cloud.pl
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 die unless @ARGV == 2;
 
-my ($run_dir,$sample_name)=@ARGV;
+my ($run_dir,$sample_name,$passvalue)=@ARGV;
 
 my $pindel_vcf=$run_dir."/pindel/pindel.out.raw.CvgVafStrand_pass.Homopolymer_pass.vcf"; 
 my $gatk_vcf=$run_dir."/gatk/".$sample_name.$passvalue".gatk.indel.vcf";
